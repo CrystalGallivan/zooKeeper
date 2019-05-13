995370fb2f191c33eec49330111852d4fc9132c5
Your Name <crystalgallivan@gmail.com>
2019-05-13 16:54:26 -0600

    All the Animals in the Alphabet

---
+++ Xenopus.js
@@ -0,0 +1,25 @@
+class Xenopus {
+  constructor(
+    name = '',
+    color = '',
+    gender = '',
+    weight = 0,
+    tailLengthInch = 0,
+    teethSize = '',
+  ) {
+    this.name = name;
+    this.color = color;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInch = tailLengthInch;
+
+  }
+  eat(food) {
+    console.log(`Nibble nibble all the ${food}ies`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+
+}
+export default Xenopus
---
+++ zoo.js
@@ -0,0 +1,6 @@
+import Antelope from "./antelope";
+import Cougar from "./cougar";
+
+let ally = new Antelope("Ally", 'Brown', "M", 200, 24, 'Medium')
+let bigBen = new Bear('Big Ben', 'Black', "M", 400, 4, 'Large')
+let craig = new Cougar('Craig', 'Brown', 'M', 150, 30, 'Medium')
