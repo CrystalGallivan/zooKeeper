590a3b9b6f15c540b237e173163ea39e3dab4121
Your Name <crystalgallivan@gmail.com>
2019-05-13 17:24:08 -0600

    Instances Created

--- zoo.js
+++ zoo.js
@@ -1,6 +1,50 @@
 import Antelope from "./antelope";
 import Cougar from "./cougar";
+import Dolphin from "./dolphin";
+import Elephant from "./elephant";
+import Flamingo from "./flamingo";
+import Gorilla from "./gorilla";
+import Hippopotamus from "./hippopotamus";
+import Ibis from "./ibis";
+import Jackal from "./jackal";
+import Koala from "./koala";
+import Lemur from "./lemur";
+import Manatee from "./manatee";
+import Nutria from "./nutria";
+import Otter from "./otter";
+import Quetzal from "./quetzal";
+import Reindeer from "./reindeer";
+import Seal from "./seal";
+import Toucan from "./toucan";
+import Unicorn from "./unicorn";
+import Vervet from "./vervet";
+import Wildebeest from "./wildebeest";
+import Xenopus from "./Xenopus";
+import Yak from "./yak";
+import Zebra from "./zebra";
 
 let ally = new Antelope("Ally", 'Brown', "M", 200, 24, 'Medium')
 let bigBen = new Bear('Big Ben', 'Black', "M", 400, 4, 'Large')
 let craig = new Cougar('Craig', 'Brown', 'M', 150, 30, 'Medium')
+let deliahla = new Dolphin('Deliahla', "Grey", 'F', 300, 45, "Small")
+let elly = new Elephant("Elly", 'Grey', 'F', 1200, 36, 'Big')
+let francis = new Flamingo("Francis", 'Pink', 'M', 40, 2, 'None')
+let gabby = new Gorilla("Gabby", "Black", "F", 355, 2, "Medium")
+let harry = new Hippopotamus("Harry", "Grey", "M", 900, 4, "Big")
+let irene = new Ibis("Irene", "Brown", "F", 20, 1, "None")
+let jackie = new Jackal("Jackie", 'Tan', "F", 140, 3, 'Medium')
+let kelly = new Koala("Kelly", "Grey", "F", 30, 6, 'Small')
+let louise = new Lemur("Lemur", "Black", "M", 20, 18, "Small")
+let mary = new Manatee("Mary", "Grey", "F", 1200, 12, "Medium")
+let noah = new Nutria('Noah', 'Brown', 'M', 10, 6, 'Small')
+let opie = new Otter('Opie', "Brown", 'M', 18, 16, 'Small')
+let quinn = new Quetzal('Quinn', 'Brown', 'F', 2, 0, 'Extra Small')
+let reeve = new Reindeer('Reeve', 'Brown', 'M', 170, 4, "Medium")
+let scotty = new Seal('Scotty', "White", "M", 450, 40, "Large")
+let tyler = new Toucan("Tyler", 'Black', "M", 7, 3, 'None')
+let ulysses = new Unicorn('Ulysses', 'Rainbow', "M", 180, 30, "Medium")
+let velvet = new Vervet("Velvet", 'Brown', 'M', 30, 6, 'Small')
+let wally = new Wildebeest("Wally", 'Brown', "M", 220, 30, "Medium")
+let xeno = new Xenopus('Xeno', "Brown", 'M', 1, 0, "Extra Small")
+let yeti = new Yak("Yeti", "Brown", 'M', 300, 20, 'Large')
+let zolia = new Zebra("Zolia", 'Black', 'M', 170, 20, "Medium")
